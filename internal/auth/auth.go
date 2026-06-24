672234cc6bf32e9b71ca58202b2f758ff2d746ce
Jurre <j.boellen@poraad.nl>
2026-06-24 11:33:04 +0200

    bug fix

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -19,7 +19,5 @@ func GetAPIKey(headers http.Header) (string, error) {
 		return "", errors.New("malformed authorization header")
 	}
 
-    return "kaas", nil
-
-// 	return splitAuth[1], nil
+	return splitAuth[1], nil
 }
